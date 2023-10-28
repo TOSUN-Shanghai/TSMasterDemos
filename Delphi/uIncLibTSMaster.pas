c4bdd8940db8c2bbcb760b37073b9fafd1c2279a
LC03 <freshhope@126.com>
2023-10-27 22:41:04 -0400

    new api to open help file chapter

--- Delphi/uIncLibTSMaster.pas
+++ Delphi/uIncLibTSMaster.pas
@@ -2166,6 +2166,7 @@ function lin_active_frame_in_schedule_table(const AChnIdx: uint32; const AID: by
 function lin_deactive_frame_in_schedule_table(const AChnIdx: uint32; const AID: byte; const AIndex: int32): integer; stdcall; {$IFNDEF LIBTSMASTER_IMPL} external DLL_LIB_TSMASTER; {$ENDIF}
 function flexray_disable_frame(const AChnIdx: int32; const ASlot: byte; const ABaseCycle: byte; const ACycleRep: byte; const ATimeoutMs: int32): integer; stdcall; {$IFNDEF LIBTSMASTER_IMPL} external DLL_LIB_TSMASTER; {$ENDIF}
 function flexray_enable_frame(const AChnIdx: int32; const ASlot: byte; const ABaseCycle: byte; const ACycleRep: byte; const ATimeoutMs: int32): integer; stdcall; {$IFNDEF LIBTSMASTER_IMPL} external DLL_LIB_TSMASTER; {$ENDIF}
+function open_help_doc(const AFileNameWoSuffix: pansichar; const ATitle: pansichar): integer; stdcall; {$IFNDEF LIBTSMASTER_IMPL} external DLL_LIB_TSMASTER; {$ENDIF}
 // MP DLL function import end (do not modify this line)
 
 {$ENDIF}
